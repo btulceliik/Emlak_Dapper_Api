73c298c45a394a06eac61f847ffa652d1bc99eff
btulceliik <128741070+btulceliik@users.noreply.github.com>
2024-02-29 04:03:34 +0300

    6.Gün Staj Sonu Çalışması

---
+++ Emlak_Dapper_UI/ViewComponents/AnaSayfa/_DefaultServicesComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Emlak_Dapper_UI.ViewComponents.AnaSayfa
+{
+    public class _DefaultServicesComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+
+        }
+    }
+}
---
+++ Emlak_Dapper_UI/ViewComponents/AnaSayfa/_DefaultWhoWeAreCompanentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Emlak_Dapper_UI.ViewComponents.AnaSayfa
+{
+    public class _DefaultWhoWeAreCompanentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+            
+        }
+    }
+}
